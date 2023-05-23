559bb97cc7d62c9c868ad1fd0471ad0719225a97
SachinPandey582 <sachinpandey582@gmail.com>
2023-05-23 11:18:42 +0530

    added the timer

---
+++ companyassignment1/src/components/data.jsx
@@ -0,0 +1,274 @@
+const ArrayOfImages=()=>{
+     return (
+        [
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/17R6UR9sjkE/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCcFiycu1w7RXlSArPkBdyKC8qqcg",
+              url: "https://www.youtube.com/watch?v=17R6UR9sjkE",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/b4R2qtmvbqc/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDMKMdH9F1OgaRlR2c_L3Blm81Nmg",
+              url: "https://www.youtube.com/watch?v=b4R2qtmvbqc",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },{
+              thumbnail:
+                "https://i.ytimg.com/vi/17R6UR9sjkE/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCcFiycu1w7RXlSArPkBdyKC8qqcg",
+              url: "https://www.youtube.com/watch?v=17R6UR9sjkE",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/b4R2qtmvbqc/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDMKMdH9F1OgaRlR2c_L3Blm81Nmg",
+              url: "https://www.youtube.com/watch?v=b4R2qtmvbqc",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+            {
+              thumbnail:
+                "	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q",
+              url: "https://www.youtube.com/watch?v=XPYLlrZ9UZs",
+            },
+            {
+              thumbnail:
+                "https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ",
+              url: "https://www.youtube.com/watch?v=yMEoDk2M7M0",
+            },
+        
+            // Add more objects for additional videos
+          ]
+     )
+}
+
+export default ArrayOfImages
--- companyassignment1/src/pages/assignment2.jsx
+++ companyassignment1/src/pages/assignment2.jsx
@@ -1,79 +1,55 @@
-import { Box, Center } from "@chakra-ui/react";
-import React, { useState } from "react";
-import ReactPlayer from "react-player/youtube";
-const ReactPlayer1 = () => {
-    const [currentVideo, setCurrentVideo] = useState('https://www.youtube.com/watch?v=17R6UR9sjkE');
-    const [thumbnails, setThumbnails] = useState([
-        {
-          thumbnail: 'https://i.ytimg.com/vi/17R6UR9sjkE/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCcFiycu1w7RXlSArPkBdyKC8qqcg',
-          url: 'https://www.youtube.com/watch?v=17R6UR9sjkE',
-        },
-        {
-          thumbnail: 'https://i.ytimg.com/vi/b4R2qtmvbqc/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDMKMdH9F1OgaRlR2c_L3Blm81Nmg',
-          url: 'https://www.youtube.com/watch?v=b4R2qtmvbqc',
-        },
-        {
-          thumbnail: 'https://i.ytimg.com/vi/yMEoDk2M7M0/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBZOpOLVcTRQaqQHcrHWAW2ZkI9RA  ',
-          url: 'https://www.youtube.com/watch?v=yMEoDk2M7M0',
-        },
-        {
-          thumbnail: '	https://i.ytimg.com/vi/XPYLlrZ9UZs/hqdefault.jpg?s…CBlKGIwDw==&rs=AOn4CLDXnp58zSzRd13EgDjdzrY6cOgH0Q',
-          url: 'https://www.youtube.com/watch?v=XPYLlrZ9UZs',
-        },
-       
-        // Add more objects for additional videos
-      ]);
-  
-    const handleThumbnailClick = (videoUrl) => {
-        console.log(videoUrl)
-      setCurrentVideo(videoUrl);
-    };
+
+
+
+
+import React, { useState } from 'react';
+import ReactPlayer from 'react-player';
+import "../Styles/Player.css"
+import ArrayOfImages from '../components/data.jsx';
+import { Box, Center, Heading, Image } from '@chakra-ui/react';
+
+
+
+const VideoPage = () => {
+  const [currentVideo, setCurrentVideo] = useState(
+        "https://www.youtube.com/watch?v=17R6UR9sjkE"
+      );
+      const [play, setplay] = useState(false);
+      const [thumbnails, setThumbnails] = useState(ArrayOfImages);
+    
+      const ThumbnailClick = (videoUrl) => {
+        console.log(videoUrl);
+        setplay(true);
+        setCurrentVideo(videoUrl);
+      };
   return (
     <div>
+     
+      <Center position={"fixed"} top={2} ml={"20%"}>
+       <ReactPlayer url={currentVideo} playing={play} playIcon />
+     </Center>
+     
+      
+        <div className="thumbnail_container">
+          
 
-        {/* if i pass them as sigle url then its normal and when i did the array they became
-        a playlist  */}
-      <Center>
-        <ReactPlayer url={
-    currentVideo
-    }  playIcon/>
-      </Center>
-      <Box>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          overflowX: 'scroll',
-          marginTop: '20px',
-        }}
-      >
-        {thumbnails.map((thumbnailUrl, index) => (
-          <div
-            key={index}
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              width: '300px',
-              height: '150px',
-              borderRadius: '50%',
-              border: '2px solid #000',
-              margin: '0 10px',
-              cursor: 'pointer',
-            }}
-            onClick={() => handleThumbnailClick(thumbnailUrl.url)}
-          >
-            <img
-              src={thumbnailUrl.thumbnail}
-              alt={`Thumbnail ${index + 1}`}
-              style={{ width: '100%', height: '150px', borderRadius: '50%' }}
+        {thumbnails.map((el, index) => (
+          <Box className='circles' onClick={() => ThumbnailClick(el.url)} key={index}>
+            <Image
+             w={"100px"}
+             h={"100px"}
+              borderRadius={"50%"}
+              src={el.thumbnail}
+              alt="thumbnail"
             />
-          </div>
+          </Box>
         ))}
-      </div>
-      </Box>
+
+
+        </div>
+      
     </div>
   );
 };
 
-export default ReactPlayer1;
+export default VideoPage;
